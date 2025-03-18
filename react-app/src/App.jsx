dfa30fdac7c12a1347a075f729441d8e69079e43
gerome <geromesinoy14@gmail.com>
2025-03-18 13:29:08 +0800

    Install tailwindcss assets styles

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,43 +1,12 @@
-import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-
-function Card({title}){
-  const [count, setCount] = useState(0);
-
-  const [hasLiked, setHasLiked] = useState(false);
-
-  useEffect(() => {
-    console.log(`${title} has been liked: ${hasLiked}`)
-  },[hasLiked] );
-
-   
-  return(
-    <>
-    <div className='card' onClick={() => setCount( count + 1)}>
-      <h2>{title} {count ? count: null}</h2>
-
-      <button onClick={() => setHasLiked(!hasLiked)}>
-        {hasLiked? '✅' : '❌'}
-      </button>
-    </div>
-    </ >
-  )
-
-}
-
-function App() {
-
+const App = () => {
   return (
     <>
-      <div className='card-container'>
-        <Card title='Star Wars'/>
-        <Card title='Avatar'/>
-        <Card title='Lion King'/>
-      </div>
-
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    
     </>
   )
 }
