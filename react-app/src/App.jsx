ae508d029ad3bf6b20b720a59cf855d004543e14
gerome <geromesinoy14@gmail.com>
2025-03-18 11:52:52 +0800

    useEffect Hook

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,16 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 
 function Card({title}){
+  const [count, setCount] = useState(0);
+
   const [hasLiked, setHasLiked] = useState(false);
-  
+
+  useEffect(() => {
+    console.log(`${title} has been liked: ${hasLiked}`)
+  },[hasLiked] );
+
+   
   return(
     <>
-    <div className='card'>
-      <h2>{title}</h2>
+    <div className='card' onClick={() => setCount( count + 1)}>
+      <h2>{title} {count ? count: null}</h2>
 
       <button onClick={() => setHasLiked(!hasLiked)}>
         {hasLiked? '✅' : '❌'}
